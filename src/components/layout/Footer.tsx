062524aaaa57203c2b38aeee78cece03210a47b0
27smehta <162454939+27smehta@users.noreply.github.com>
2025-05-22 10:06:04 -0400

    removed some unwanted files

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Mail, Instagram, Youtube, Linkedin } from "lucide-react";
+import { Mail, Instagram, Youtube, Linkedin, QrCode } from "lucide-react";
 
 const Footer = () => {
   return (
